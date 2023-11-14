ed826add46e2e38b6bed0636043b5cfeb1d6eccd
Vladyslav Aviedov <vladaviedov@protonmail.com>
2023-11-13 19:16:16 -0500

    shell input loop, catch sigint

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
 
 #include "vector.h"
 #include "vars.h"
@@ -10,26 +13,33 @@ int main() {
 	extern const char **environ;
 	vars_import(environ);
 
-	char buffer[1024] = {0};
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+
+	char buffer[1024];
 	uint32_t index = 0;
 
-	printf("$ ");
-	int ch;
-	while ((ch = fgetc(stdin)) != '\n' && ch != EOF) {
-		buffer[index] = ch;
-		index++;
+	while (1) {
+		memset(buffer, 0, 1024);
+		index = 0;
+
+		printf("$ ");
+		int ch;
+		while ((ch = fgetc(stdin)) != '\n' && ch != EOF) {
+			buffer[index] = ch;
+			index++;
+		}
+
+		char *subbed_str = parser_sub(buffer);
+		str_vec *parsed_str = parser_split(subbed_str);
+		for (uint32_t i = 0; i < parsed_str->count; i++) {
+			printf("%u: %s\n", i, *(char **)vec_at(parsed_str, i));
+		}
+
+		run_dispatch(parsed_str);
+		vec_free_with_elements(parsed_str);
+		free(subbed_str);
 	}
-
-	char *subbed_str = parser_sub(buffer);
-	str_vec *parsed_str = parser_split(subbed_str);
-	for (uint32_t i = 0; i < parsed_str->count; i++) {
-		printf("%u: %s\n", i, *(char **)vec_at(parsed_str, i));
-	}
-
-	run_dispatch(parsed_str);
-
-	vec_free_with_elements(parsed_str);
-	free(subbed_str);
 
 	return 0;
 }
--- src/run.c
+++ src/run.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <signal.h>
 
 #include "vars.h"
 
